5a620d390048c9af5e382e40070cc7b83b97837e
Dylan F3rnand3z <202021090@tese.edu.mx>
2023-07-17 20:51:20 -0600

    Modificacion gafete v4

--- GenerarGafete.php
+++ GenerarGafete.php
@@ -12,7 +12,7 @@ class PDF extends FPDF
 function Header()
 {
     // Logo
-    $this->Ln(30);
+    $this->Ln(45);
     $this->Image('Imagenes/Logo_tese.jpg',165,8,20);
     //$this->Image('Imagenes/EdoMex.png',11,8,40);
     $this->Image('Imagenes/EdoMex.png',11,8,20);
@@ -22,7 +22,7 @@ function Header()
     // Movernos a la derecha
     $this->Cell(80);
     // Título
-    $this->Cell(30,10,'Evento Dual 2023', 0, 0, 'I');
+    $this->Cell(30,10,'Evento Dual 2023', 0, 0, 'L');
     // Salto de línea
     $this->Ln(20);
     //$this->Image('Imagenes/Logo_TECHNM.png',30,8,33);
