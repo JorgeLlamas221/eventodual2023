d1df1d0ab4f09dc5a6b7ea83a8a8e39398cf108b
Dylan F3rnand3z <202021090@tese.edu.mx>
2023-07-17 23:13:43 -0600

    Modificaciones Gafete V2.3

--- GenerarGafete.php
+++ GenerarGafete.php
@@ -94,13 +94,13 @@ while($res2 = mysqli_fetch_array($select2)){
 
     $pdf->SetTextColor(0, 0, 0);
     $pdf->SetFont('Arial','B',13);
-    $pdf->cell(85, 30, $res2['nombres']." ".$res2['apellidoPaterno']." ".$res2['apellidoMaterno'], 1, 0, 'C');
+    $pdf->cell(85, 25, $res2['nombres']." ".$res2['apellidoPaterno']." ".$res2['apellidoMaterno'], 1, 0, 'C');
     $pdf->Ln(); //
-    $pdf->cell(85, 30, $res2['nombreEmpresa'], 1, 0, 'C');
+    $pdf->cell(85, 15, $res2['nombreEmpresa'], 1, 0, 'C');
     $pdf->Ln();
     $pdf->SetFont('Arial','',13);
     $pdf->cell(85, 30, "Junio 2023", 1, 0, 'R', 1);
-    $pdf->Image('Codigo_QR/QR.png',11, 85, 30);
+    $pdf->Image('Codigo_QR/QR.png',21, 85, 30); // 11, 85, 30
     $pdf->Ln();
 
     $pdf->SetFillColor(175, 34, 34);
