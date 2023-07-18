ff3db08b7c0086f668b7fd90261e8b4684700cb4
Dylan F3rnand3z <202021090@tese.edu.mx>
2023-07-18 01:45:43 -0600

    Modificacion Mensaje Login - Usuario V1.2

--- loginP.php
+++ loginP.php
@@ -42,8 +42,8 @@
             <button id="btn__iniciar-sesion">Iniciar Sesión</button>
             </div>
             <div class="caja__trasera-register">
-                <h3>Nombre de Usuario: usuario1</h3>
-                <h3>Contraseña: 123456789</h3>
+                <h3>Nombre de Usuario: admin</h3>
+                <h3>Contraseña: Admin123</h3>
             </div>
         </div>
         <!--Formulario Login-->
